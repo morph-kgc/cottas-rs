1cfb9dfa07094b3c1282b126ceaaf864d3651d68
savacano28 <stephanya.casanova@filigran.io>
2025-12-11 00:17:39 +0100

    feat(rdf2cottas): fix export and test

--- src/export.rs
+++ src/export.rs
@@ -2,18 +2,17 @@ use crate::utils::build_order_by;
 use duckdb::Connection;
 
 pub fn export_to_cottas(conn: &Connection, index: &str, path: &str, quad_mode: bool) {
-    let order_by = build_order_by(index);
+    //let order_by = build_order_by(index);
     let select = if quad_mode {
         "SELECT DISTINCT s, p, o, g FROM quads"
     } else {
         "SELECT DISTINCT s, p, o FROM quads"
     };
 
-    let kv_metadata = format!("index={}", index);
 
     let query = format!(
-        "COPY ({} ORDER BY {}) TO '{}' (FORMAT PARQUET, COMPRESSION ZSTD, COMPRESSION_LEVEL 22, PARQUET_VERSION '2.0', kv_metadata='{}')",
-        select, order_by, path, kv_metadata
+        "COPY ({}) TO '{}' (FORMAT PARQUET, COMPRESSION ZSTD, COMPRESSION_LEVEL 22, PARQUET_VERSION 'V2')",
+        select, path
     );
 
     conn.execute(query.as_str(), []).unwrap();
