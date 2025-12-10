6a996d66b8182222f07c24dd6a22c767c83f809f
savacano28 <stephanya.casanova@filigran.io>
2025-12-11 00:05:36 +0100

    feat(rdf2cottas): fix export and test

--- src/export.rs
+++ src/export.rs
@@ -9,10 +9,14 @@ pub fn export_to_cottas(conn: &Connection, index: &str, path: &str, quad_mode: b
         "SELECT DISTINCT s, p, o FROM quads"
     };
 
+    // DuckDB solo acepta un string simple en kv_metadata
+    let kv_metadata = format!("index={}", index);
+
     let query = format!(
-        "COPY ({} ORDER BY {}) TO '{}' (FORMAT PARQUET, COMPRESSION ZSTD, COMPRESSION_LEVEL 22, PARQUET_VERSION '2.0', KV_METADATA {{index='{}'}})",
-        select, order_by, path, index
+        "COPY ({} ORDER BY {}) TO '{}' (FORMAT PARQUET, COMPRESSION ZSTD, COMPRESSION_LEVEL 22, PARQUET_VERSION '2.0', kv_metadata='{}')",
+        select, order_by, path, kv_metadata
     );
 
     conn.execute(query.as_str(), []).unwrap();
 }
+
